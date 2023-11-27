9c1e5143f48f3d8bc63a462ac45acdf58dfb69e6
ROAJ12 <roman.alvarado.juan.carlos@gmail.com>
2023-11-26 23:36:32 -0600

    Server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const server = http.createServer();
+
+const io = require('socket.io')(server, {
+    cors: { origin: '*' }
+});
+
+const users = {};
+
+io.on("connection", (socket) => {
+    // Maneja la asignación de nombres de usuario
+    socket.on("set_username", (username) => {
+        users[socket.id] = username;
+        console.log(`${username} connected with ID: ${socket.id}`);
+    });
+
+    socket.on("chat_message", (body) => {
+        // Modifica el evento para incluir el nombre de usuario en lugar de la ID del socket
+        const username = users[socket.id] || "Anonimo";
+        socket.broadcast.emit("chat_message", {
+            body,
+            from: username,
+        });
+    });
+});
+
+server.listen(5555);
